213509efb20a950d34cedf806dd9a6e060fc9232
Konfermat <semyon.obuhov@gmail.com>
2025-11-16 17:32:23 +0700

    shame

--- homework/trafic_system/will remake later or never/main.py
+++ homework/trafic_system/will remake later or never/main.py
@@ -2,8 +2,8 @@ from trafic_system_sim import *
 
 if __name__ == '__main__':
     road1 = Road()
-    car1 = Car(0, 1, 5)
-    car2 = Car(0, 2, 3)
+    car1 = Car(0, 1, 3)
+    car2 = Car(1, 1, 3)
 
     simulation = TrafficFlowSimulator(road1)
     simulation.add_vehicle(car1)
--- homework/trafic_system/will remake later or never/trafic_system_sim.py
+++ homework/trafic_system/will remake later or never/trafic_system_sim.py
@@ -3,19 +3,6 @@ import os
 import uuid
 from abc import ABC, abstractmethod
 
-# road_len = 20
-# road_left = list('0' * road_len)
-# road_right = list('0' * road_len)
-# car = '#'
-# def road_sim():
-#     for i in range(len(road_right)):
-#         road_right[i] = car
-#         print(*road_right)
-#         road_right[i] = '0'
-#         time.sleep(1)
-#         os.system('cls' if os.name == 'nt' else 'clear')
-
-# содержит
 class Road:
     def __init__(self, length=20):
         self.length = length
@@ -36,34 +23,48 @@ class Vehicle(ABC):
         self.speed = self.speed - 1
         return self.speed
     @abstractmethod
-    def decide_action(self):
+    def decide_action(self, road):
         pass
 
 class Car(Vehicle):
-    def decide_action(self):
-        return 1
+    def decide_action(self, road):
+        p = self.position
+        r_p = []
 
-# управляет трафиком
+        # for i in range(len(road.vehicles)):
+        #     road.dir_right[road.vehicles[i].position] = '░'
+
 class TrafficFlowSimulator:
     def __init__(self, road):
         self.road = road
     def add_vehicle(self, vehicle):
         self.road.vehicles.append(vehicle)
     def run_cycle(self):
-        self.road.vehicles.pop(0)
-        for i in range(len(self.road.vehicles)):
-            print(self.road.vehicles[i])
 
 
-        # for i in range(self.road.length):
-        #     os.system('cls' if os.name == 'nt' else 'clear')
-        #     # дорога
-        #     print('-' * self.road.length, sep='')
-        #     print(*self.road.dir_left[::-1], sep='')
-        #     print('- ' * int(self.road.length / 2), sep='')
-        #     print(*self.road.dir_right[::-1], sep='')
-        #     print('-' * self.road.length, sep='')
-        #     time.sleep(0.1)
+
+
+
+        for i in range(self.road.length):
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+            for i in range(len(self.road.vehicles)):
+                self.road.vehicles[i].decide_action(self.road)
+                self.road.dir_right[self.road.vehicles[i].position] = '#'
+
+            # дорога
+            print('-' * self.road.length, sep='')
+            print(*self.road.dir_left[::-1], sep='')
+            print('- ' * int(self.road.length / 2), sep='')
+            print(*self.road.dir_right, sep='')
+            print('-' * self.road.length, sep='')
+
+            for i in range(len(self.road.vehicles)):
+                self.road.dir_right[self.road.vehicles[i].position] = '░'
+
+            time.sleep(0.1)
+
+
 # --------------------
 # ░░░░░░░░░░░░░░░░░░░░
 # - - - - - - - - - -
